b2ff20fb8c690b6307561b6308e04e5e4fabf38e
Jason Cardwell <cardwell.jason@gmail.com>
2016-06-13 10:51:35 -0700

    House keeping and re-implemented Stack
    Signed-off-by: Jason Cardwell <cardwell.jason@gmail.com>

--- Frameworks/MoonKit/Moonkit/Data Structures/Stack.swift
+++ Frameworks/MoonKit/Moonkit/Data Structures/Stack.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-public struct Stack<T>: CollectionType, CustomStringConvertible, ArrayLiteralConvertible {
+public struct CompositeStack<T>: CollectionType, CustomStringConvertible, ArrayLiteralConvertible {
 
   private var storage: [T]
 
@@ -58,11 +58,281 @@ public struct Stack<T>: CollectionType, CustomStringConvertible, ArrayLiteralCon
   /**
   reversed
 
-  - returns: Stack<T>
+  - returns: CompositeStack<T>
   */
-  public func reversed() -> Stack<T> { return Stack<T>(Array(storage.reverse())) }
+  public func reversed() -> CompositeStack<T> { return CompositeStack<T>(Array(storage.reverse())) }
 
   public var description: String { return storage.description }
 
-  public init(arrayLiteral elements: T...) { self = Stack<T>(elements) }
+  public init(arrayLiteral elements: T...) { self = CompositeStack<T>(elements) }
 }
+
+public struct Stack<Element> {
+  private typealias Storage = StackStorage<Element>
+  private typealias Buffer = StackBuffer<Element>
+
+  public typealias _Element = Element
+  public typealias Generator = StackGenerator<Element>
+  public typealias SubSequence = Stack<Element>
+
+  private var buffer: Buffer
+
+  public var count: Int { return buffer.count }
+  public var capacity: Int { return buffer.capacity }
+
+  public var isEmpty: Bool { return count == 0 }
+
+  private mutating func ensureUnique() {
+    ensureUniqueWithCapacity(capacity)
+  }
+
+  public var reversed: Stack<Element> { var result = self; result.buffer.reverse(); return result }
+
+  public mutating func reverse() { ensureUnique(); buffer.reverse() }
+
+  private func cloneBuffer(minimumCapacity minimumCapacity: Int) -> Buffer {
+    var clone = Buffer(minimumCapacity: minimumCapacity)
+    clone.elements.initializeFrom(buffer.elements, count: buffer.count)
+    clone.endIndex = buffer.endIndex
+    return clone
+  }
+
+  private mutating func ensureUniqueWithCapacity(requiredCapacity: Int) {
+
+    switch (isUnique: buffer.isUniquelyReferenced(), hasCapacity: buffer.capacity >= requiredCapacity) {
+
+      case (isUnique: true, hasCapacity: true):
+        return
+
+      case (isUnique: true, hasCapacity: false):
+        buffer = cloneBuffer(minimumCapacity: requiredCapacity * 2)
+
+      case (isUnique: false, hasCapacity: true):
+        buffer = cloneBuffer(minimumCapacity: capacity)
+
+      case (isUnique: false, hasCapacity: false):
+        buffer = cloneBuffer(minimumCapacity: requiredCapacity * 2)
+    }
+
+  }
+
+  public init() {
+    self = Stack<Element>(minimumCapacity: 0)
+  }
+
+  public init(minimumCapacity: Int) {
+    buffer = Buffer(minimumCapacity: minimumCapacity)
+  }
+
+  public init<S:SequenceType where S.Generator.Element == Element>(_ sequence: S) {
+    let elements = sequence as? Array<Element> ?? Array(sequence)
+    var stack = Stack<Element>(minimumCapacity: elements.count)
+    for element in sequence { stack.buffer.push(element) }
+    self = stack
+  }
+
+  public mutating func pop() -> Element? {
+    ensureUnique()
+    return buffer.pop()
+  }
+
+  public var peek: Element? { return buffer.peek }
+
+  public mutating func push(element: Element) {
+    ensureUniqueWithCapacity(count + 1)
+    buffer.push(element)
+  }
+
+}
+
+extension Stack: ArrayLiteralConvertible {
+  public init(arrayLiteral elements: Element...) {
+    self = Stack<Element>(elements)
+  }
+}
+
+extension Stack: SequenceType {
+
+  public func generate() -> Generator { return Generator(stack: self) }
+
+  public func dropFirst(n: Int) -> Stack<Element> {
+    var result = self
+    for _ in 0 ..< n { _ = result.buffer.pop() }
+    return result
+  }
+
+  public func dropLast(n: Int) -> Stack<Element> {
+    var result = self
+    result.buffer.reverse()
+    result.dropFirst(n)
+    result.buffer.reverse()
+    return result
+  }
+
+  public func prefix(maxLength: Int) -> Stack<Element> {
+    var result = Stack<Element>(minimumCapacity: maxLength)
+    for element in self {
+      guard result.count < maxLength else { break }
+      result.buffer.push(element)
+    }
+    result.buffer.reverse()
+    return result
+  }
+
+  public func suffix(maxLength: Int) -> Stack<Element> {
+    var result = Stack<Element>(minimumCapacity: maxLength)
+    for i in 0 ..< min(count, maxLength) {
+      result.push(buffer.elements[i])
+    }
+    return result
+  }
+
+  public func split(maxSplit: Int,
+                    allowEmptySlices: Bool,
+                    @noescape isSeparator: (Element) throws -> Bool) rethrows -> [Stack<Element>]
+  {
+    var result: [Stack<Element>] = []
+    var currentStack = Stack<Element>()
+
+    for element in self {
+      if !(try isSeparator(element)) {
+        currentStack.push(element)
+      } else {
+        currentStack.buffer.reverse()
+        result.append(currentStack)
+        currentStack = Stack<Element>()
+      }
+    }
+
+    if currentStack.count > 0 {
+      currentStack.buffer.reverse()
+      result.append(currentStack)
+    }
+
+    return result
+  }
+}
+
+extension Stack: CustomStringConvertible, CustomDebugStringConvertible {
+
+  public var description: String {
+    guard count > 0 else { return "[]" }
+    var result = "["
+    var first = true
+    for element in self {
+      if first { first = false } else { result += ", " }
+      print(element, terminator: "", toStream: &result)
+    }
+    return result
+  }
+
+  public var debugDescription: String {
+    guard count > 0 else { return "[]" }
+    var result = "["
+    var first = true
+    for element in self {
+      if first { first = false } else { result += ", " }
+      debugPrint(element, terminator: "", toStream: &result)
+    }
+    return result
+  }
+
+}
+
+public struct StackGenerator<Element>: GeneratorType {
+  private let elements: UnsafeMutablePointer<Element>
+  private var top: Int
+
+  private init(stack: Stack<Element>) {
+    elements = stack.buffer.elements
+    top = stack.buffer.endIndex.predecessor()
+  }
+
+  public mutating func next() -> Element? {
+    guard top > -1 else { return nil }
+    let result = (elements + top).memory
+    top = top &- 1
+    return result
+  }
+}
+
+private struct StackStorageHeader {
+  let capacity: Int
+  var count: Int
+}
+
+private final class StackStorage<Element>: ManagedBuffer<StackStorageHeader, UInt8> {
+  typealias Header = StackStorageHeader
+  typealias Storage = StackStorage<Element>
+
+  class func bytesForElements(capacity: Int) -> Int {
+    return strideof(Element) * capacity
+  }
+
+  var capacity: Int { return value.capacity }
+  var count: Int { get { return value.count } set { value.count = newValue } }
+
+  var elements: UnsafeMutablePointer<Element> { return withUnsafeMutablePointerToElements { UnsafeMutablePointer<Element>($0) } }
+
+  class func create(minimumCapacity: Int) -> StackStorage {
+    let requiredCapacity = bytesForElements(minimumCapacity)
+    let storage = super.create(requiredCapacity) { _ in Header(capacity: minimumCapacity, count: 0) }
+    return storage as! Storage
+  }
+
+  deinit {
+    guard !_isPOD(Element) else { return }
+    elements.destroy(count)
+  }
+}
+
+private struct StackBuffer<Element> {
+  typealias Storage = StackStorage<Element>
+  typealias Buffer = StackBuffer<Element>
+
+  var count: Int { return endIndex }
+  var capacity: Int { return storage.capacity }
+  var storage: Storage
+
+  @inline(__always) mutating func isUniquelyReferenced() -> Bool { return Swift.isUniquelyReferenced(&storage) }
+
+  let elements: UnsafeMutablePointer<Element>
+
+  let startIndex = 0
+  var endIndex = 0 {
+    didSet { storage.count = endIndex }
+  }
+
+  var peek: Element? {
+    guard endIndex > 0 else { return nil }
+    return (elements +  endIndex.predecessor()).memory
+  }
+
+  mutating func push(element: Element) {
+    (elements + endIndex).initialize(element)
+    endIndex = endIndex &+ 1
+  }
+
+  mutating func pop() -> Element? {
+    guard endIndex > 0 else { return nil }
+    let result = (elements + endIndex.predecessor()).move()
+    endIndex = endIndex &- 1
+    return result
+  }
+
+  mutating func reverse() {
+    guard endIndex > 1 else { return }
+    let indices = 0 ..< endIndex
+    for (leftIndex, rightIndex) in zip(indices, indices.reverse()) {
+      guard leftIndex < rightIndex else { break }
+      swap(&elements[leftIndex], &elements[rightIndex])
+    }
+  }
+
+  init(minimumCapacity: Int) {
+    storage = Storage.create(minimumCapacity)
+    elements = storage.elements
+  }
+
+}
+
