52d8424e5dadc0fb2ed47715f058ea37b4ebcdbc
Jason Cardwell <cardwell.jason@gmail.com>
2016-06-24 08:51:21 -0700

    removing MoonKit to be added back as submodule
    Signed-off-by: Jason Cardwell <cardwell.jason@gmail.com>

--- Frameworks/MoonKit/Moonkit/MoonKit.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import MoonKit
-
-let url = NSBundle.mainBundle().URLForResource("E7ECBB95-CFB8-46BF-AA31-4CC62230D817_TestSummaries", withExtension: "plist")!
-let dictionary = NSDictionary(contentsOfURL: url) as! Dictionary<String, AnyObject>
-
-
-print(dictionary)
-
--- Frameworks/MoonKit/Moonkit/Parsing/XMLParser.swift
+++
@@ -1,9 +0,0 @@
-//
-//  XMLParser.swift
-//  MoonKit
-//
-//  Created by Jason Cardwell on 8/5/15.
-//  Copyright © 2015 Jason Cardwell. All rights reserved.
-//
-
-import Foundation
